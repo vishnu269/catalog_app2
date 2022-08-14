0ba70733efa2cf63a800e1bd88d4e1e431ce44aa
FlutLab.io <>
2022-08-14 00:10:01 +0000

    Autosave

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,31 @@
 import "package:flutter/cupertino.dart";
 
 import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
 import 'package:fluttercatalog/models/catalog.dart';
 import 'package:fluttercatalog/widgets/drawer.dart';
 import 'package:fluttercatalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString('assets/files/catalog.json');
+    print(catalogJson);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
